913fada5fa1ba357ad7da8a0a90af66e87d90c79
Enhao Gong <gongenhao@gmail.com>
2017-03-13 14:36:10 -0700

    add setup_inputs_two_src

--- srez_input.py
+++ srez_input.py
@@ -29,17 +29,57 @@ def setup_inputs(sess, filenames, image_size=None, capacity_factor=3):
     image = tf.random_crop(image, [crop_size, crop_size, 3])
 
     image = tf.reshape(image, [1, crop_size, crop_size, 3])
-    image = tf.cast(image, tf.float32)/255.0
+    image = tf.cast(image, tf.float32)/255.0 # from 0~1
 
     if crop_size != image_size:
         image = tf.image.resize_area(image, [image_size, image_size])
-
+ 
     # The feature is simply a Kx downscaled version
     K = 4
     downsampled = tf.image.resize_area(image, [image_size//K, image_size//K])
 
     feature = tf.reshape(downsampled, [image_size//K, image_size//K, 3])
     label   = tf.reshape(image,       [image_size,   image_size,     3])
+
+    # Using asynchronous queues
+    features, labels = tf.train.batch([feature, label],
+                                      batch_size=FLAGS.batch_size,
+                                      num_threads=4,
+                                      capacity = capacity_factor*FLAGS.batch_size,
+                                      name='labels_and_features')
+
+    tf.train.start_queue_runners(sess=sess)
+      
+    return features, labels
+
+def setup_inputs_MRI(sess, filenames_input, filenames_output, image_size=None, capacity_factor=3):
+
+    if image_size is None:
+        image_size = FLAGS.sample_size
+    
+    # Read each JPEG file
+    reader_input = tf.WholeFileReader()
+    filename_queue_input = tf.train.string_input_producer(filenames_input)
+    key, value_input = reader_input.read(filename_queue_input)
+    channels = 3
+    image_input = tf.image.decode_jpeg(value_input, channels=channels, name="input_image")
+    image_input.set_shape([None, None, channels])
+
+    # read output
+    reader_output = tf.WholeFileReader()
+    filename_queue_output = tf.train.string_input_producer(filenames_output)
+    key, value_output = reader_output.read(filename_queue_output)
+    channels = 3
+    image_output = tf.image.decode_jpeg(value_output, channels=channels, name="output_image")
+    image_output.set_shape([None, None, channels])
+
+    # cast
+    image_input = tf.cast(image_input, tf.float32)/255.0
+    image_output = tf.cast(image_output, tf.float32)/255.0
+
+    # The feature is simply a Kx downscaled version
+    feature = tf.reshape(image_input, [image_size, image_size, 3])
+    label   = tf.reshape(image_output,       [image_size,   image_size,     3])
 
     # Using asynchronous queues
     features, labels = tf.train.batch([feature, label],
